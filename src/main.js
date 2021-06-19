b92ad50ce9b5b3345ef922436229f7c082a94d44
MTHLily <tree.of.pyres@gmail.com>
2021-06-19 23:22:20 +0900

    Fixed styling by adding reset.css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "./assets/css/reset.css";
 import "./assets/css/style.css";
 import "./assets/css/link_hotspot.css";
 import "./assets/css/info_hotspot.css";
